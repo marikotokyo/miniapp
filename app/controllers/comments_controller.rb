dfc95aa660114465978f983676d39fc91891e484
marikotokyo <marikokanefuji@MarikonoMacBook-ea.local>
2018-11-24 11:49:41 +0900

    Add mini app

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    redirect_to "/"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(article_id: params[:article_id],user_id: current_user.id)
+  end
+end
---
+++ vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/marikokanefuji/Desktop/material_sample/miniapp/vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
